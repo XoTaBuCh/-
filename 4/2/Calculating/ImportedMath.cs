ac92c604760abdbd603b6227abb6662a3154b87c
MAX <58146871+XoTaBuCh@users.noreply.github.com>
2021-04-02 15:13:15 +0300

    Update ImportedMath.cs

--- 4/2/Calculating/ImportedMath.cs
+++ 4/2/Calculating/ImportedMath.cs
@@ -17,16 +17,16 @@ namespace Calculating
         [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern float Divide(float a, float b);
 
-        [DllImport("MathLib.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Power(int a, int b);
 
-        [DllImport("MathLib.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Factorial(int a);
 
-        [DllImport("MathLib.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int GCD(int a, int b);
 
-        [DllImport("MathLib.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("MathLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int LCM(int a, int b);
     }
 }
